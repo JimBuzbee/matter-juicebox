cd977821768e99c6ab09c949807a734cd6087463
Jim Buzbee <Jim.Buzbee@gmail.com>
2024-10-26 15:11:49 -0600

    Refactor and first attempt to send commands to Juicebox

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-
 //---------------------------------------------------------------------------------------------------------------------------------
 
 import {DeviceTypeId, Endpoint, Environment, ServerNode, StorageService, Time, VendorId,} from "@matter/main";
 import { LevelControlServer } from "@matter/main/behaviors/level-control";
 import { TemperatureSensorDevice } from "@matter/main/devices/temperature-sensor";
 import { OnOffPlugInUnitDevice } from "@matter/main/devices";
+import { DimmablePlugInUnitDevice } from "@matter/main/devices";
 
 import net from 'net';
 import * as dgram from "dgram";
@@ -21,6 +21,11 @@ const argv : any = yargs(process.argv).parse();
 const juiceBoxIP = argv.juicebox_host; // e.g. '192.168.1.107'; 
 const juiceBoxTelnetPort = argv.juicebox_telnet_port ?? 2000;
 const juiceBoxPollSeconds = argv.juicebox_poll_seconds ?? 30;
+
+const COMMAND_PORT = 3000; // 8047;
+
+// Create a UDP socket
+const commandClient = dgram.createSocket('udp4');
 
 // host and port of the Matter handler 
 const matterHost = argv.matter_host; //'192.168.1.86';
@@ -73,11 +78,13 @@ async function main() {
 
     // Create a Matter "endpoint" - a component of a node. Add optional output Level Control 
     // there are choices here regarding what type of Matter device(s) or Bridge could be used 
-    const plugEndpoint = new Endpoint( OnOffPlugInUnitDevice.with(LevelControlServer) , { id: "onoff" });
+ //  const plugEndpoint = new Endpoint( OnOffPlugInUnitDevice.with(LevelControlServer) , { id: "onoff" });
+    const plugEndpoint = new Endpoint( DimmablePlugInUnitDevice, { id: "onoff" });
+
     await node.add( plugEndpoint );
 
     // FIXME - should be set from 1st JuiceBox UDP message
-    plugEndpoint.set({ levelControl: { minLevel: 0 }}); 
+    plugEndpoint.set({ levelControl: { minLevel: 1 }}); // FIXME - need to specify feature LT=0 (?) to allow 0 here 
     plugEndpoint.set({ levelControl: { maxLevel: 40 }}); 
 
     // JuiceBox also reports temperature
@@ -86,6 +93,10 @@ async function main() {
     await node.add( temperatureEndpoint );
 
     plugEndpoint.events.onOff.onOff$Changed.on(value => {
+        const m = new Message()   
+        m.time = moment();
+        // .......
+        const builtMessage = m.build();
          // FIXME - send message to JuiciBox
         console.log(`OnOff is now ${value ? "ON" : "OFF"}`);
     });
@@ -97,10 +108,16 @@ async function main() {
         m.offline_amperage = 20; // FIXME - what shoud go here?
         m.instant_amperage = value ?? 0;
         const builtMessage = m.build();
-        console.log(builtMessage);
-        console.log(m.inspect());
+        console.log("message = " + builtMessage);
+        console.log( m.inspect() );
 
-        // FIXME - send message to JuiciBox
+        const message = Buffer.from(builtMessage);
+
+        // Send the message
+        commandClient.send(message, 0, message.length, COMMAND_PORT, juiceBoxIP, (err) => {
+            if (err) console.error('Error sending message:', err);
+            else console.log('Message sent to JuiceBox:', juiceBoxIP, COMMAND_PORT);
+        });
 
         console.log(`Plug level is now ${value}`);
     });
@@ -134,37 +151,32 @@ function telnetJuiceBox( ) {
             // from our prespective, connection is open
             opened = true;
 
-            console.log('***************************************** Telnet connection to JuiceBox ');
+            console.log('Telnet connection to JuiceBox ');
 
             // list JuiceBox connections - will parse below when received
             client.write('list\n');
             
-            client.on('data', (data: Buffer) => { // response from the server
-                
-                if ( opened ) { // we only care if we consider connection open
-                    const array: string[] = data.toString().split('\n').filter(line => line.trim() !== '');
-
-                    for ( const line of array ) {
-                        if ( line.includes('UDPC')  ) {                         
-                            if ( !line.includes( matterHost ) ) {
-                                // FIXME - should parse out connection handle rather than hard-code '2'
-                                client.write(`close 2\nudpc ${matterHost} ${matterPort}\nexit\n`);
-                                console.log('Closing connection after UDPC change');                               
-                            } else  console.log('Closing connection after no change needed');  
-
+            client.on('data', (data: Buffer) => {            
+                if ( opened ) { // only if we consider connection open
+                    for ( const line of data.toString().split('\n').filter(line => line.trim() !== '') ) {
+                        if ( line.includes('UDPC') ) {                         
+                            if ( ! line.includes( matterHost ) ) {  // e.g. "# 2 UDPC  jbv1.emotorwerks.com:8042 (13191)"
+                                client.write(`close ${line.split(' ')[1]}\nudpc ${matterHost} ${matterPort}\nexit\n`);
+                                console.log('UDPC change');                               
+                            }
                             opened = false;
                             client.end(); 
                             break;
                         }
                     }
-                 }
+                }
             });
        });  
     } 
 }
 //---------------------------------------------------------------------------------------------------------------------------------
 
-function setupUdp( plugEndpoint: Endpoint<OnOffPlugInUnitDevice>, 
+function setupUdp( plugEndpoint: Endpoint<DimmablePlugInUnitDevice>, 
     temperatureEndpoint: Endpoint<TemperatureSensorDevice> ) {
   
         var udpserver = dgram.createSocket('udp4');
@@ -181,7 +193,7 @@ function setupUdp( plugEndpoint: Endpoint<OnOffPlugInUnitDevice>,
 
             // Set Matter attributes
             plugEndpoint.set({ onOff: { onOff: (packet.power > 0)}}); 
-        //  plugEndpoint.set({ levelControl: { maxLevel: packet.current_setting }} ); 
+            plugEndpoint.set({ levelControl: { maxLevel: packet.current_setting }} ); 
 
             temperatureEndpoint.set({ temperatureMeasurement: { measuredValue: ( packet.temperature - 32) * 5/9 * 100 }}); 
       });
@@ -193,18 +205,10 @@ async function getConfiguration() {
    * Collect all needed data
    *
    * This block collects all needed data from cli, environment or storage. Replace this with where ever your data come from.
-   *
-   * Note: This example uses the matter.js process storage system to store the device parameter data for convenience
-   * and easy reuse. When you also do that be careful to not overlap with Matter-Server own storage contexts
-   * (so maybe better not do it ;-)).
    */
   const environment = Environment.default;
 
   const storageService = environment.get(StorageService);
-  console.log(`Storage location: ${storageService.location} (Directory)`);
-  console.log(
-      'Use the parameter "--storage-path=NAME-OR-PATH" to specify a different storage location in this directory, use --storage-clear to start with an empty storage.',
-  );
   const deviceStorage = (await storageService.open("device")).createContext("data");
 
 
@@ -214,7 +218,7 @@ async function getConfiguration() {
   const discriminator = environment.vars.number("discriminator") ?? (await deviceStorage.get("discriminator", 3840));
   // product name / id and vendor id should match what is in the device certificate
   const vendorId = environment.vars.number("vendorid") ?? (await deviceStorage.get("vendorid", 0xfff1));
-  const productName = `node-matter JuiceBox ${"Socket"}`;
+  const productName = `matter.js JuiceBox`;
   const productId = environment.vars.number("productid") ?? (await deviceStorage.get("productid", 0x8000));
 
   const port = environment.vars.number("port") ?? 5540;
